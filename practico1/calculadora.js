44d40e3fc9224b4fb32ee8267712e5236844ca35
1708pmg <gutierrezpm61@gmail.com>
2023-08-14 23:08:27 -0300

    primera calculadora

---
+++ practico1/calculadora.js
@@ -0,0 +1,46 @@
+function Calcular(){
+    let num1 = parseFloat(document.getElementById("num1").value);
+    let operador = document.getElementById("opciones").value
+    let num2 = parseFloat(document.getElementById("num2").value);
+    let resultado = document.getElementById("resultado");
+    
+    //si los operandos no son numeros o estan vacios alerta sobre el error//
+    if (isNaN(num1) || isNaN(num2)){
+        alert ("Ingrese numeros");
+        return resultado.textContent = "Error";
+    }
+   
+    resultado = 0
+    switch (operador) {
+        case "suma":
+            resultado = num1 + num2;
+            break;
+        case "resta":
+            resultado = num1 - num2;
+            break;
+        case "multiplicacion":
+            resultado = num1 * num2;
+            break;
+        case "division":
+            //Si el usuario divide por cero, muestra un mensaje indicando error//
+            if (num2 !== 0){
+                resultado = num1 / num2 ;}
+                else { 
+                    resultado = "Error, no se puede dividir por 0";}
+            break;
+    }
+
+    document.getElementById("resultado").innerHTML = resultado;
+}
+//Si el resultado de una operación es demasiado grande o pequeño indica error//
+/*(resultado === Infinity{
+        resultado = "El resultado es demasiado grande";}
+        else if ( resultado === -Infinity){
+            resultado = "El resultado es demasiado pequeño";
+        }*/
+function borrar(){
+    document.getElementById("num1").value = "";
+    document.getElementById("num2").value = "";
+    document.getElementById("resultado").innerHTML = "Resultado";
+    document.getElementById("miCalculadora").reset;
+}
---
+++ practico1/readme.md
@@ -0,0 +1,32 @@
+# Calculadora simple
+Se ha realizado como trabajo practico N° 1 en el marco de Programa de Paginas Webs con componentes de Argentina Programa, desarrollado por la Universidad Nacional de Córdoba
+
+##  Caracteristicas de la calculadora 🚀
+Es una calculadora simple que realiza las cuatro operaciones principales: suma, resta, multiplicacion y division.
+Posee dos visores para ingresar los operandos deseados. 
+Tiene un menu desplegable donde se puede elegir al operador. Un boton para calcular y otro para borrar o limpiar los datos ingresados. 
+
+## Fuentes de apoyo ⚙️
+He mirado muchos tutoriales en youtibe, leido articulos de varias paginas de desarrollo web que proveen muchos tutoriales. Por momentos es tan abrumadora la cantidad de informacion que se hace muy dificil enumerar las distintas fuentes de informacion.
+Dejo algunas solo como ejemplo: 
+https://www.w3schools.com/html/
+https://www.w3schools.com/css/default.asp
+https://www.w3schools.com/js/default.asp
+https://developer.mozilla.org/es/docs/Web/JavaScript
+
+
+### Pruebas🔩
+He realizdo el proyecto en base a ensayo y error. 
+He usado el atributo number para evitar que el usaurio ponga otra cosa que no sea un numero, tambien se ha creado un alerta, que si no ingresa ningun valor y quiere realizar una operacion le aparezca un alerta informandole. 
+En el caso de dividir por cero, se le informara al usuario que no es una operacion valida.
+
+## Licencia 📄
+
+Este proyecto fue realizado por Patricia Gutierrez
+
+## Expresiones de Gratitud 🎁
+
+* Agradezco a mis profesores Ignacio Moretti y Nehuen Aramburu. Ademas a los compañeros de curso que ayudan con sus comentarios y preguntas.
+
+
+
